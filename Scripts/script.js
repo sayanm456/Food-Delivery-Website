8fd257867519fbc5edf80655d95b9a8d03ba9381
Sayan <sayanm2345@gmail.com>
2024-09-17 00:32:51 +0530

    modifying styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 
-var boxes = document.body.getElementsByClassName("box")
+const boxes = document.body.getElementsByClassName("box")
+const len = document.body.getElementsByClassName("box").length
+const btn = document.body.getElementsByClassName("btn")
 
-for (let i = 0; i < 4; i++) {
-    boxes[i].style.borderColor = "green"   
+for (let i = 0; i <= len; i++) {
+    boxes[i].style.borderColor = "green"  
+    boxes[i].style.backgroundColor = "lightblue"
 }
 
+
+btn[0].addEventListener("click", function() {
+    alert("Clicked!")
+})
