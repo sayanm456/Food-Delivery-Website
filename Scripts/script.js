4b61ce9ac96c038425baeae36d5fc8be73e04dc9
Sayan <sayanm2345@gmail.com>
2024-09-16 22:31:42 +0530

    adding styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,8 @@
-console.log("This is script.js")
+"use client"
+
+var boxes = document.body.getElementsByClassName("box")
+
+for (let i = 0; i < 4; i++) {
+    boxes[i].style.borderColor = "green"   
+}
+
